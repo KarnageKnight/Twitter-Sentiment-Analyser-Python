6f2785970af134cb11ff1d42bbbd92a10775acc8
KarnageKnight <karanvir95@gmail.com>
2017-02-24 23:47:55 +0530

    Fixed the words not being searched problem. New max_id and since_id method used

--- mapper.py
+++ mapper.py
@@ -21,12 +21,12 @@ def NLTK(js, i, count):
 		polarity_file.write('negative\t')
 		polarity_file.write(json.dumps(polarityData['probability']['neg'])) #write NLTK negative results to file
 		polarity_file.write('\n') #new line
-	# elif(polarityData['label']=='neutral'):
-	# 	polarity_file.write('neutral\t')
-	# 	polarity_file.write(json.dumps(polarityData['probability']['neutral'])) #write NLTK neutral results to file
-
+	else:
+		polarity_file.write('neutraly\t')
+		polarity_file.write(json.dumps(polarityData['probability']['neutral'])) #write NLTK neutral results to file
+		polarity_file.write('\n') #new line
     
-
+#TODO: Add retweet again
 
 threadArray = [] #all the threads will be kept in this array, to be joined later
 TWITTER_URL = 'https://api.twitter.com/1.1/search/tweets.json?'
@@ -36,9 +36,9 @@ f = open("/users/karnageknight/Desktop/majorProject/Data/search_result.txt","a+"
 count = 0
 for x in range(0,3):   #range defines number of pages displayed
 	if x==0:
-		url = twurl.augment(TWITTER_URL, {'q': word_no_retweet, 'count': '100', 'lang': 'en'} ) #for first page
+		url = twurl.augment(TWITTER_URL, {'q': word_no_retweet, 'count': '100', 'lang': 'en', 'return_type': 'recent'} ) #for first page
 	else:
-		url = twurl.augment(TWITTER_URL,{'q' : word_no_retweet, 'count': '100', 'lang': 'en', 'include_entities': 'true', 'max_id' : next_max_id}) #for subsequent pages using their max_id
+		url = twurl.augment(TWITTER_URL,{'q' : word_no_retweet, 'count': '100', 'lang': 'en', 'include_entities': 'true', 'since_id' : next_max_id, 'return_type': 'recent'}) #for subsequent pages using their max_id
 	#print 'Retrieving', url
 	#print word_no_retweet
 	print ''
@@ -47,13 +47,8 @@ for x in range(0,3):   #range defines number of pages displayed
 	headers = connection.info().dict #store all headers and their values in data type dictionary 
     
 	js = json.loads(data) #read data from a file type format and convert it to json format
-	try:
-		next_results_url_params = js['search_metadata']['next_results'] #extract parameter next_results from search_metadata
-		next_max_id = next_results_url_params.split('max_id=')[1].split('&')[0] #extract max_id of next page from next_results
-	except:
-		print 'couldn\'t get max ID for next page'
-        # No more next pages
-		break
+	# print json.dumps(js,indent = 4)
+	next_max_id = str(json.dumps(js['search_metadata']['max_id']-1)) #extract parameter next_results from search_metadata
 
 	print 'Remaining', headers['x-rate-limit-remaining']
 	print 'loop'
--- reducer.py
+++ reducer.py
@@ -9,7 +9,7 @@ with open("/Users/karnageknight/Desktop/majorProject/Data/polarity_file.txt","r+
 		count = float(count)
 		if word == 'positive':	
 			total_count = total_count+count
-		else:
+		elif word == 'negative':
 			total_count = total_count-count
 
 	final_file = open("/Users/karnageknight/Desktop/majorProject/Data/final_file.txt","w+")
@@ -20,7 +20,7 @@ with open("/Users/karnageknight/Desktop/majorProject/Data/polarity_file.txt","r+
 		final_file.write(str(total_count))
 	else:
 		print '%s\t%s' % ('negative',(total_count*(-1)))
-		final_file.write('positive\t')
-		final_file.write(str(total_count*1))
+		final_file.write('negative\t')
+		final_file.write(str(total_count*-1))
 
 	final_file.close()
