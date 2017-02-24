4019419b39f2b5497af7a8270f0cb9e72b14297b
KarnageKnight <karanvir95@gmail.com>
2017-02-24 23:49:56 +0530

    comments fixed

--- mapper.py
+++ mapper.py
@@ -13,6 +13,8 @@ def NLTK(js, i, count):
 	data2 = urllib.urlencode({'text': str(tweet)})  #encode tweet in proper format for API call
 	u = urllib.urlopen('http://text-processing.com/api/sentiment/', data2) #perform NLTK API call
 	polarityData=json.loads(u.read()) #get polarity
+
+	#writing the mapper file for the reducer input
 	if(polarityData['label']=='pos'):
 		polarity_file.write('positive\t')
 		polarity_file.write(json.dumps(polarityData['probability']['pos'])) #write NLTK positive results to file
@@ -26,8 +28,6 @@ def NLTK(js, i, count):
 		polarity_file.write(json.dumps(polarityData['probability']['neutral'])) #write NLTK neutral results to file
 		polarity_file.write('\n') #new line
     
-#TODO: Add retweet again
-
 threadArray = [] #all the threads will be kept in this array, to be joined later
 TWITTER_URL = 'https://api.twitter.com/1.1/search/tweets.json?'
 word = raw_input('Enter word:')
@@ -38,7 +38,7 @@ for x in range(0,3):   #range defines number of pages displayed
 	if x==0:
 		url = twurl.augment(TWITTER_URL, {'q': word_no_retweet, 'count': '100', 'lang': 'en', 'return_type': 'recent'} ) #for first page
 	else:
-		url = twurl.augment(TWITTER_URL,{'q' : word_no_retweet, 'count': '100', 'lang': 'en', 'include_entities': 'true', 'since_id' : next_max_id, 'return_type': 'recent'}) #for subsequent pages using their max_id
+		url = twurl.augment(TWITTER_URL,{'q' : word_no_retweet, 'count': '100', 'lang': 'en', 'include_entities': 'true', 'since_id' : next_max_id, 'return_type': 'recent'}) #for subsequent tweets using their max_id
 	#print 'Retrieving', url
 	#print word_no_retweet
 	print ''
@@ -48,7 +48,7 @@ for x in range(0,3):   #range defines number of pages displayed
     
 	js = json.loads(data) #read data from a file type format and convert it to json format
 	# print json.dumps(js,indent = 4)
-	next_max_id = str(json.dumps(js['search_metadata']['max_id']-1)) #extract parameter next_results from search_metadata
+	next_max_id = str(json.dumps(js['search_metadata']['max_id']-1)) #it is the next_max_id for the next result
 
 	print 'Remaining', headers['x-rate-limit-remaining']
 	print 'loop'
